6802ff3157924ee82a0973a41245cd25147ef1de
mobin <mobinarmandei07@gmail.com>
2025-03-28 00:48:19 +0430

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+
+const token = '7932955419:AAGotXY-CrZo1Nod9V5C0B80KcjDaZSdNTg';
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+
+    bot.sendMessage(chatId, 'سلام به ربات خوش آمدید.');
+});
