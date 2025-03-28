336286b34319ee09a0df239b9797a59c1876c6c4
mobin <mobinarmandei07@gmail.com>
2025-03-28 23:48:49 +0430

    finish

--- index.js
+++ index.js
@@ -5,12 +5,29 @@ const token = '7932955419:AAGotXY-CrZo1Nod9V5C0B80KcjDaZSdNTg';
 const bot = new TelegramBot(token, { polling: true });
 
 bot.on('message', (msg) => {
-
+    let definition = false;
     const chatId = msg.chat.id;
     const userText = msg.text;
 
     if(userText == '/start') {
-        bot.sendMessage(chatId, 'سلام به ربات خوش آمدید.');
+        definition = true;
+        bot.sendMessage(chatId, 'سلام به ربات من خوش آمدید.',
+            {
+                "reply_markup":{
+                    "keyboard":[
+                        ['گزینه1', 'گزینه2'],
+                        ['گزینه3', 'گزینه4']
+                    ]
+                }
+            });
+    }
+
+    if(userText == 'salam' || userText == 'سلام') {
+        btn.sendMessage(chatId, 'سلام کاربر گرامی');
+    }
+
+    if(!definition) {
+        bot.sendMessage(chatId, 'دستور ارسال شده تعریف نشده است.');
     }
 
 });
