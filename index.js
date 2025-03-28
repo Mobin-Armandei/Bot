b8eb1e94afef76945c0a4eceb1c8707c8d83230e
mobin <mobinarmandei07@gmail.com>
2025-03-28 21:36:17 +0430

    fixed

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ const token = '7932955419:AAGotXY-CrZo1Nod9V5C0B80KcjDaZSdNTg';
 const bot = new TelegramBot(token, { polling: true });
 
 bot.on('message', (msg) => {
-    console.log(msg);
-    // const chatId = msg.chat.id;
 
-    // bot.sendMessage(chatId, 'سلام به ربات خوش آمدید.');
+    const chatId = msg.chat.id;
+    const userText = msg.text;
+
+    if(userText == '/start') {
+        bot.sendMessage(chatId, 'سلام به ربات خوش آمدید.');
+    }
+
 });
